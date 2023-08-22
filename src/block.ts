8d34e3a372301dc7193440f446bc4768374e8fec
dev-rafa1707 <rafa1707@gmail.com>
2023-08-21 22:56:07 -0300

    constructor

--- src/block.ts
+++ src/block.ts
@@ -1,6 +1,11 @@
 export default class Block {
-    index: number = 1;
-    hash: string = "";
+    index: number;
+    hash: string;
+
+    constructor(index: number, hash: string) {
+        this.index = index;
+        this.hash = hash;
+    }
 
     isValid(): boolean{
         if (this.index < 0) return false;
--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,8 +1,8 @@
 import Block from "./block";
 
-const block1 = new Block();
-block1.hash = "a";
-block1.index = 1;
+const block1 = new Block(1, "a");
+// block1.hash = "a";
+// block1.index = 1;
 
 
 console.log(block1)
