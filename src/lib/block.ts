bb72e0af9c8172d227fc6da0b01a7587bc0c2520
dev-rafa1707 <rafa1707@gmail.com>
2023-08-22 23:07:27 -0300

    improve class block

--- src/lib/block.ts
+++ src/lib/block.ts
@@ -1,10 +1,18 @@
+
+
 export default class Block {
     index: number;
+    timestamp: number;
     hash: string;
+    previousHash: string;
+    data: string;
 
-    constructor(index: number, hash: string) {
+    constructor(index: number, hash: string, previousHash: string, data: string) {
         this.index = index;
         this.hash = hash;
+        this.timestamp = Date.now();
+        this.previousHash = previousHash;
+        this.data = data
     }
 
     isValid(): boolean{
