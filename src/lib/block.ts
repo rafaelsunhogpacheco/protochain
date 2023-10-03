6d2c93efce433010d0a90e6a729b105370605e2e
dev-rafa1707 <rafa1707@gmail.com>
2023-10-02 22:16:53 -0300

    add nounce and miner as block atributes

--- src/lib/block.ts
+++ src/lib/block.ts
@@ -24,12 +24,19 @@ export default class Block {
         return sha256(this.index + this.data + this.timestamp + this.previousHash + this.nonce + this.miner).toString();
     }
 
-    isValid(previousHash: string, previousIndex: number): Validation{
+    isValid(previousHash: string, previousIndex: number, dificulty: number): Validation{
         if (previousIndex !== this.index - 1) return new Validation(false, "Invalid Index");
         if (this.hash !== this.getHash()) return new Validation(false, "Invalid Hash");
         if (!this.data) return new Validation(false, "Invalid data");
         if (this.timestamp < 1) return new Validation(false, "Invalid Timestamp");
         if (this.previousHash !== previousHash) return new Validation(false, "Invalid Previous Hash");
+        if (!this.nonce || !this.miner) return new Validation(false, "No mined.")
+
+        const prefix = new Array(dificulty + 1).join("0");
+        if (this.hash !== this.getHash() || this.hash.startsWith(prefix))
+            return new Validation(false, "Invalid hash.");
+
+
         return new Validation();
     }
 }
