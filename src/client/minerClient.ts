2c942fa7c577608ab1160c19bb3e41ccc90575a0
dev-rafa1707 <rafa1707@gmail.com>
2023-10-23 20:07:41 -0300

    add blockInfo to minerClient

--- src/client/minerClient.ts
+++ src/client/minerClient.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
+import BlockInfo from "../lib/blockInfo";
 
 const BLOCKCHAIN_SERVER = 'http://localhost:3000/';
 
 async function mine() {
+    console.log('Getting next block info...');
     const { data } = await axios.get(`${BLOCKCHAIN_SERVER}blocks/next`);
+    const blockInfo = data as BlockInfo;
+    
     console.log(data)
 }
 
