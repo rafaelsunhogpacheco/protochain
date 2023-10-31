16181c4403138a5beccc7c1a5a256276daf7f23d
dev-rafa1707 <rafa1707@gmail.com>
2023-10-31 19:59:52 -0300

    set not to mine without transactions

--- src/client/minerClient.ts
+++ src/client/minerClient.ts
@@ -17,6 +17,13 @@ let totalMined = 0;
 async function mine() {
     console.log('Getting next block info...');
     const { data } = await axios.get(`${BLOCKCHAIN_SERVER}blocks/next`);
+    if(!data) {
+        console.log('No block to mine. Waiting 5 seconds...');
+        return setTimeout(() => {
+            mine();
+        }, 5000)
+    }
+    
     const blockInfo = data as BlockInfo;
 
     const newBlock = Block.fromBlockInfo(blockInfo);
